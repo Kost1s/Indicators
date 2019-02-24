46052ac4434de3e62b35e0dea138aad483c8a5a6
Kost1s <antonioucostis@gmail.com>
2019-02-24 14:32:09 +0000

    code tidy

--- src/main/java/com/kap/indicators/vwap/MultipleMovingVWAP.java
+++ src/main/java/com/kap/indicators/vwap/MultipleMovingVWAP.java
@@ -7,16 +7,15 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-
 /**
- * Created by Kostis on 3/4/2017.
+ * @author Konstantinos Antoniou
  */
-
 public class MultipleMovingVWAP {
 
-    private MathContext mathContext = new MathContext(6, RoundingMode.DOWN);
+    private final MathContext mathContext = new MathContext(6, RoundingMode.DOWN);
 
-    public final Map<Integer, Map<BigDecimal, BigDecimal>> getMultipleMovingVWAP(List<Trade> tradesList, List<Integer> timeIntervals) {
+    public final Map<Integer, Map<BigDecimal, BigDecimal>> getMultipleMovingVWAP(List<Trade> tradesList,
+                                                                                 List<Integer> timeIntervals) {
         Map<Integer, Map<BigDecimal, BigDecimal>> multipleMovingVWAP = new LinkedHashMap<>();
 
         for (int timeInterval : timeIntervals) {
@@ -26,55 +25,44 @@ public class MultipleMovingVWAP {
     }
 
     private Map<BigDecimal, BigDecimal> getMovingVWAP(List<Trade> tradesList, int timeInterval) {
-
         Map<BigDecimal, BigDecimal> movingVWAP = new LinkedHashMap<>();
-        Long timeIntervalL = timeInterval * 1000L;
+        long timeIntervalL = timeInterval * 1000L;
         BigDecimal timeDiffFromSecondZero;
 
         int endingPoint;
         Long endingPointTime;
         for (int startingPoint = 0; startingPoint < tradesList.size(); startingPoint++) {
-
             endingPointTime = tradesList.get(startingPoint).getTradeTime().getTime() + timeIntervalL;
             endingPoint = getEndingPoint(tradesList, endingPointTime);
 
             if (endingPoint != -1) {
-                timeDiffFromSecondZero = BigDecimal.valueOf((tradesList.get(endingPoint).getTradeTime().getTime() - tradesList.get(0).getTradeTime().getTime()) / 1000D);
+                timeDiffFromSecondZero = BigDecimal.valueOf((tradesList.get(endingPoint).getTradeTime().getTime() -
+                                                             tradesList.get(0).getTradeTime().getTime()) / 1000D);
                 movingVWAP.put(timeDiffFromSecondZero, getTimeIntervalVWAP(tradesList, startingPoint, endingPoint));
             }
         }
         return movingVWAP;
     }
 
-
     private BigDecimal getTimeIntervalVWAP(List<Trade> tradesList, int initialTimePoint, int finalTimePoint) {
-
-        BigDecimal timeIntervalSumSharesNoTimesPrice;
-        BigDecimal timeIntervalSharesNo;
-        BigDecimal timeIntervalVWAP;
-
-        timeIntervalSumSharesNoTimesPrice = BigDecimal.ZERO;
-        timeIntervalSharesNo = BigDecimal.ZERO;
-
+        BigDecimal timeIntervalSumSharesNoTimesPrice = BigDecimal.ZERO;
+        BigDecimal timeIntervalSharesNo = BigDecimal.ZERO;
         for (int k = initialTimePoint; k <= finalTimePoint; k++) {
-            timeIntervalSumSharesNoTimesPrice = timeIntervalSumSharesNoTimesPrice.add(tradesList.get(k).getTradeQty().multiply(tradesList.get(k).getTradePrice()));
+            timeIntervalSumSharesNoTimesPrice = timeIntervalSumSharesNoTimesPrice.add(
+                    tradesList.get(k).getTradeQty().multiply(tradesList.get(k).getTradePrice()));
             timeIntervalSharesNo = timeIntervalSharesNo.add(tradesList.get(k).getTradeQty());
         }
-        timeIntervalVWAP = timeIntervalSumSharesNoTimesPrice.divide(timeIntervalSharesNo, mathContext);
-
-        return timeIntervalVWAP;
+        return timeIntervalSumSharesNoTimesPrice.divide(timeIntervalSharesNo, mathContext);
     }
 
-
     private int getEndingPoint(List<Trade> tradesList, Long endingPointTime) {
-
         int midElement;
         int firstElement = 0;
         int lastElement = tradesList.size() - 1;
 
         while (firstElement <= lastElement) {
 
-            midElement = firstElement + (lastElement - firstElement) / 2;
+            midElement = firstElement + ((lastElement - firstElement) / 2);
             Long midElementTime = tradesList.get(midElement).getTradeTime().getTime();
 
             if (midElementTime > endingPointTime) {
